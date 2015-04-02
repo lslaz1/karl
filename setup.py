93011f14e5b5af03428661903a601a502ca034be
vangheem <vangheem@gmail.com>
2015-04-01 21:06:42 -0500

    Prepare karl 5.0.2a6.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-__version__ = '5.0.2a5'
+__version__ = '5.0.2a6'
 import os
 
 from setuptools import setup, find_packages
