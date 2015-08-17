a7afe054253e9e016cd877e040d76cab90dc9846
jeremy.martens <jeremy.martens@wildcardcorp.com>
2015-08-17 16:02:53 -0500

    change forbidden title

--- karl/views/forbidden.py
+++ karl/views/forbidden.py
@@ -7,7 +7,7 @@ from karl.utils import find_site
 def forbidden(context, request):
     site = find_site(context)
     request.session['came_from'] = request.url
-    api = TemplateAPI(context, request, 'Forbidden')
+    api = TemplateAPI(context, request, 'Secure Login')
     request.response.status = '200 OK'
     if api.userid:
         login_url = resource_url(site, request, 'login.html')
