f9a2d30f215999dda8a5ee77faeb4e9cba57a748
joel.kleier <joel.kleier@wildcardcorp.com>
2015-12-17 17:27:36 +0000

    update version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-__version__ = '5.6.0a2'
+__version__ = '5.6.0a3'
 import os
 
 from setuptools import setup, find_packages
